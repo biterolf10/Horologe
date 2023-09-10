2ed86eeb082e4d68ef31f355f7ede0954e515b03
biterolf10 <112725148+biterolf10@users.noreply.github.com>
2023-09-10 16:18:47 +0500

    0.03
    Changed commands, updated docs, new command

--- source/main.py
+++ source/main.py
@@ -1,6 +1,7 @@
 import os
 import art
 
+pos = ""
 content: str = ""
 filePath: str = ""
 log = []
@@ -16,7 +17,7 @@ def setContent():
             break
         elif inp == "[fu":
             fileUpd()
-        elif inp == "[sfp":
+        elif inp == "[fp":
             setFilePath()
         elif inp == "[u":
             undo()
@@ -28,11 +29,14 @@ def setContent():
             resetContent()
         elif inp == "[of":
             openFile()
+        elif inp == "[cp":
+            setCursorPosition()
 
         elif inp[0] == "]":
             log.append(inp)
+            
         else:
-            content += inp + "\n"
+            content = "{}{}{}".format(content[:int(pos)], inp, content[int(pos):])
             log.append(inp)
 
 def fileUpd():
@@ -79,6 +83,10 @@ def setFilePath():
     global filePath
     filePath = input("( ")
 
+def setCursorPosition():
+    global pos
+    pos = input("( ")
+
 def printLogo():
     art.tprint("Hourglass", space=1)
 
@@ -86,21 +94,24 @@ def showDocumentation():
     printLogo()
 
     print("\nFile commands")
-    print("[sfp (set file path), sets file path (must writed to save/open files) (required params: path to file)")
+    print("[fp (file path), sets file path (must writed to save/open files) (required params: path to file)")
     print("[fu (file update), saves files with content what you write")
-    print("[rc (reset content), reset all content what you writed")
     print("[of (open file), open file with path what setted with [sfp, content sets to file text")
-    print("[u (undo), delete one of needed string inside of content (required params: text to delete)")
 
     print("\nApp commands")
     print("[efa (exit from app), closes app")
     print("[cc (clear console), cleares console")
 
+    print("\nText commands")
+    print("[cp (cursor position), sets cursor position (required params: position (symbol number))")
+    print("[rc (reset content), reset all content what you writed")
+    print("[u (undo), delete one of needed string inside of content (required params: text to delete)")
+
     print("\nSymbols used in program")
-    print("\"( \"program wait until user write needed params to command")
-    print("\") \"program showed something went wrong and nothing happend from your command")
-    print("\"[ \"used to execute commands")
-    print("\"] \"comment, program ignore this line")
+    print("\"(\" program wait until user write needed params to command")
+    print("\")\" program showed something went wrong and nothing happend from your command")
+    print("\"[\" used to execute commands")
+    print("\"]\" comment, program ignore this line")
     
 
 def main():
